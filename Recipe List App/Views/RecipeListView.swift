02fc41f3fad47ae2d4aee5a7f6692662132b0fce
Gustavo Hernadnez <hernandezgustavo819@gmail.com>
2023-06-09 22:07:33 -0700

    Added RecipeFeaturedView -Modified data flow using environment object modifier -Create RecipeModel instance in RecipeTabView

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -9,43 +9,39 @@ import SwiftUI
 
 struct RecipeListView: View {
     
-    
-    //Reference view model
-    //@ObservedObject will allow to listen and look for any changes in RecipeModel 
-    @ObservedObject var model = RecipeModel()
-    
+    @EnvironmentObject var model:RecipeModel
     
     var body: some View {
         
-        
         NavigationView {
-            List(model.recipes){ r in
+            List(model.recipes) { r in
                 
-                NavigationLink {
-                    RecipeDetailView(recipe: r)
-                } label: {
-                    HStack{
-                        Image(r.image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 50, height:50, alignment: .center)
-                            .clipped()
-                            .cornerRadius(5)
-                        Text(r.name)
-
-                    }
-
-                }
-
+                NavigationLink(
+                    destination: RecipeDetailView(recipe:r),
+                    label: {
+                        
+                        // MARK: Row item
+                        HStack(spacing: 20.0) {
+                            Image(r.image)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 50, height: 50, alignment: .center)
+                                .clipped()
+                                .cornerRadius(5)
+                            Text(r.name)
+                        }
+                        
+                    })
+                
                 
                 
             }
-            .navigationTitle("All Recipes")
+            .navigationBarTitle("All Recipes")
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
     }
